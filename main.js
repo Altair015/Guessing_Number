ac82e418e749bc87f75f577c4309413df2b64c2e
Altair015 <mand23sngh@gmail.com>
2024-03-31 13:57:48 +0530

    Implemented the Game Logic to check if the user input is how far from the randomly generated number and end the game if the user make 5 wrong attempts continuously.

--- main.js
+++ main.js
@@ -1,108 +1,207 @@
 const container = document.createElement("div");
-container.setAttribute("id", "container");
+container.id = "container";
 document.body.prepend(container)
 
 const headContainer = document.createElement("section");
-headContainer.setAttribute("id", "headContainer");
+headContainer.id = "headContainer";
 container.appendChild(headContainer);
 
 const btn = document.createElement("button");
-btn.setAttribute("id", "btn");
+btn.id = "btn";
 btn.setAttribute("class", "btn-font");
 btn.textContent = "Again!";
 headContainer.appendChild(btn);
 
 const info = document.createElement("sup");
-info.setAttribute("id", "info");
+info.id = "info";
 info.setAttribute("class", "game-font");
 info.textContent = "(Between 1 & 20)";
 headContainer.appendChild(info);
 
 const titleContainer = document.createElement("section");
-titleContainer.setAttribute("id", "titleContainer");
+titleContainer.id = "titleContainer";
 container.appendChild(titleContainer);
 
 const title = document.createElement("p");
-title.setAttribute("id", "title");
+title.id = "title";
 title.setAttribute("class", "game-font title-font");
 title.textContent = "Guess My Number";
 titleContainer.appendChild(title);
 
 const markRule = document.createElement("section");
-markRule.setAttribute("id", "markRule");
+markRule.id = "markRule";
 titleContainer.after(markRule)
 
 const hrRule = document.createElement("hr");
-hrRule.setAttribute("id", "hrRule");
+hrRule.id = "hrRule";
 markRule.appendChild(hrRule)
 
 const mark = document.createElement("p");
-mark.setAttribute("id", "mark");
+mark.id = "mark";
 mark.setAttribute("class", "mark-font");
 mark.textContent = "?";
 markRule.appendChild(mark);
 
 const checkScoreContainer = document.createElement("section");
-checkScoreContainer.setAttribute("id", "checkScoreContainer");
+checkScoreContainer.id = "checkScoreContainer";
 container.appendChild(checkScoreContainer);
 
 const checkContainer = document.createElement("div");
-checkContainer.setAttribute("id", "checkContainer");
+checkContainer.id = "checkContainer";
 checkScoreContainer.appendChild(checkContainer);
 
 const checkNumber = document.createElement("input");
-checkNumber.setAttribute("id", "checkNumber");
+checkNumber.id = "checkNumber";
 checkNumber.setAttribute("class", "game-font no-spinner");
-checkNumber.setAttribute("type", "number");
-checkNumber.setAttribute("value", "00");
+checkNumber.type = "number";
+checkNumber.placeholder = "00"
 checkContainer.appendChild(checkNumber);
 
 const checkBtn = document.createElement("button");
-checkBtn.setAttribute("id", "checkBtn");
+checkBtn.id = "checkBtn";
 checkBtn.setAttribute("class", "btn-font");
 checkBtn.textContent = "Check!";
 checkContainer.appendChild(checkBtn);
 
 const scoreContainer = document.createElement("div");
-scoreContainer.setAttribute("id", "scoreContainer");
+scoreContainer.id = "scoreContainer";
 checkScoreContainer.appendChild(scoreContainer);
 
 const startContainer = document.createElement("p");
-startContainer.setAttribute("id", "startContainer");
+startContainer.id = "startContainer";
 startContainer.setAttribute("class", "game-font");
 startContainer.textContent = "start guessing...";
 scoreContainer.appendChild(startContainer);
 
 const playerScore = document.createElement("p");
-playerScore.setAttribute("id", "playerScore");
+playerScore.id = "playerScore";
 playerScore.setAttribute("class", "game-font");
-playerScore.textContent = " Score: "
+playerScore.innerHTML = " Score&nbsp;&nbsp;&nbsp;&nbsp;: "
 scoreContainer.appendChild(playerScore);
 
 const playerScoreIcon = document.createElement("span");
-playerScoreIcon.setAttribute("id", "playerScoreIcon");
+playerScoreIcon.id = "playerScoreIcon";
 playerScoreIcon.setAttribute("class", "material-symbols-outlined");
 playerScoreIcon.textContent = "license";
 playerScore.prepend(playerScoreIcon);
 
 const playerScoreValue = document.createElement("span");
-playerScoreValue.setAttribute("id", "playerHighScoreValue");
+playerScoreValue.id = "playerScoreValue";
 playerScoreValue.textContent = "000000000000";
 playerScore.appendChild(playerScoreValue);
 
 const playerHighScore = document.createElement("p");
-playerHighScore.setAttribute("id", "playerHighScore");
+playerHighScore.id = "playerHighScore";
 playerHighScore.setAttribute("class", "game-font");
 playerHighScore.innerHTML = " Highscore: "
 scoreContainer.appendChild(playerHighScore);
 
 const playerHighScoreIcon = document.createElement("span");
-playerHighScoreIcon.setAttribute("id", "playerHighScoreIcon");
+playerHighScoreIcon.id = "playerHighScoreIcon";
 playerHighScoreIcon.setAttribute("class", "material-symbols-outlined");
-playerHighScoreIcon.textContent = "military_tech";
+playerHighScoreIcon.textContent = "workspace_premium";
+// playerHighScoreIcon.textContent = "military_tech";
+// playerHighScoreIcon.textContent = "social_leaderboard";
 playerHighScore.prepend(playerHighScoreIcon);
 
 const playerHighScoreValue = document.createElement("span");
-playerHighScoreValue.setAttribute("id", "playerHighScoreValue");
+playerHighScoreValue.id = "playerHighScoreValue";
 playerHighScoreValue.textContent = "000000000000";
 playerHighScore.appendChild(playerHighScoreValue);
+
+function generateNumber() {
+    return Math.trunc((Math.random() * 20) + 1);
+}
+
+let guessNumber = generateNumber();
+
+let oldGuess = "0";
+
+let guessCount = 0;
+
+btn.addEventListener("click",
+    () => {
+        playerScoreIcon.style.color = "red"
+        checkNumber.disabled = "";
+        checkBtn.disabled = "";
+        container.style.backgroundColor = "#222222";
+        playerScoreValue.textContent = "000000000000";
+        mark.textContent = "?";
+        startContainer.textContent = "start guessing...";
+        repeatedTask();
+    }
+)
+
+function gameOver() {
+    setTimeout(() => {
+        startContainer.textContent = "Game Lost.Try Again.";
+    }, 1001);
+    repeatedTask()
+    playerScoreIcon.style.color = "#222222"
+    checkNumber.disabled = "true";
+    checkBtn.disabled = "true";
+    container.style.backgroundColor = "red";
+    mark.textContent = guessNumber;
+    guessCount = 0;
+
+}
+
+function repeatedTask() {
+    checkNumber.value = "";
+    guessNumber = generateNumber();
+}
+
+function scoreUpdate(scoreVal) {
+    scoreVal.textContent = `${+scoreVal.textContent + 1}`;
+    const score = "000000000000";
+    const scoreLength = scoreVal.textContent.length;
+    const lastScore = score.slice(scoreLength);
+    const actualScore = lastScore + scoreVal.textContent;
+    if (scoreVal.textContent > playerHighScoreValue.textContent) {
+        playerHighScoreValue.textContent = actualScore;
+    }
+    return actualScore;
+}
+
+function guessStatus(tempStatus, guessIncrement = 1) {
+    startContainer.textContent = tempStatus;
+    setTimeout(() => {
+        startContainer.textContent = "start guessing...";
+    }, 1000);
+    guessCount += guessIncrement;
+}
+
+checkBtn.addEventListener("click",
+    () => {
+        if (guessCount > 4) {
+            gameOver();
+            return;
+        }
+        if (checkNumber.value > 20 || checkNumber.value < 0 || checkNumber.value == "") {
+            checkNumber.value = "";
+            guessStatus("Invalid Number");
+        }
+        else if (guessNumber == +checkNumber.value) {
+            playerScoreValue.textContent = scoreUpdate(playerScoreValue);
+            repeatedTask();
+            oldGuess = "0";
+            checkNumber.value = "";
+            guessCount = 0;
+        }
+        else {
+            const Oldabs = Math.abs(guessNumber - oldGuess);
+            const Cheabs = Math.abs(guessNumber - checkNumber.value);
+            if (Oldabs > Cheabs) {
+                guessStatus("Getting Closer.");
+            }
+            else if (Oldabs < Cheabs) {
+                guessStatus("Going Away.");
+            }
+            else if (oldGuess === checkNumber.value) {
+                guessStatus("Same Number.")
+            }
+            oldGuess = checkNumber.value;
+            checkNumber.value = "";
+        }
+    }
+)
